d98d03b9d759cc29b0f159d5ac5cf0e5ed2f174f
matheus o blasfemia <142417170+theusroma@users.noreply.github.com>
2025-02-28 09:54:56 -0300

    Create ex2.java

---
+++ 3 Semestre/estruturas condicionais/switch/ex2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        int dia;
+
+        System.out.println("Digite o mês: ");
+        dia = leitor.nextInt();
+
+        switch (dia){
+            case 1:
+                System.out.println("Janeiro");
+                break;
+
+            case 2:
+                System.out.println("Fevereiro");
+                break;
+
+            case 3:
+                System.out.println("Março");
+                break;
+
+            case 4:
+                System.out.println("Abril");
+                break;
+
+            case 5:
+                System.out.println("Maio");
+                break;
+        }
+
+
+
+    }
+}
