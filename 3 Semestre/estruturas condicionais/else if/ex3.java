06528b8927704d618331ab9e49598dabd1d3a2d7
matheus o blasfemia <142417170+theusroma@users.noreply.github.com>
2025-02-28 10:56:31 -0300

    Create ex3.java
    conversor de temperatura

---
+++ 3 Semestre/estruturas condicionais/else if/ex3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        float celsius;
+        int opcao;
+        float faren;
+        float kelvin;
+
+        System.out.println("Digite a temperatura em celsius: ");
+        celsius = leitor.nextInt();
+
+        System.out.println("Escolha a conversão: 1  - Farenheit    2 - Kelvin ");
+        opcao = leitor.nextInt();
+
+        if (opcao == 1){
+            faren = (celsius * (1.8f)) + 32;
+            System.out.println("A temperatura em Farenheit é de: " + faren);
+        }
+        else if (opcao == 2){
+            kelvin = celsius + 273f;
+            System.out.println("A temperatura em Kelvin é de: " + kelvin);
+        }
+
+
+    }
+}
