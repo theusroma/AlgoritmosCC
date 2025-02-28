27307a310ac2781af13fff822161aac3cb79fdc4
matheus o blasfemia <142417170+theusroma@users.noreply.github.com>
2025-02-28 09:42:39 -0300

    Create ex1.java

---
+++ 3 Semestre/estruturas condicionais/switch/ex1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        int dia = 1;
+
+        switch (dia){
+            case 1:
+                System.out.println("Domingo");
+                break;
+
+            case 2:
+                System.out.println("Segunda");
+                break;
+
+            case 3:
+                System.out.println("Terça");
+                break;
+        }
+
+
+
+    }
+}
