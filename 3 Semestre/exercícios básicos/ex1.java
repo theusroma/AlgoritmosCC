ef3f68b5be0452d544c972a7f17497abfb7188e4
matheus o blasfemia <142417170+theusroma@users.noreply.github.com>
2025-02-28 08:58:53 -0300

    Update ex1.java

--- 3 Semestre/exercícios básicos/ex1.java
+++ 3 Semestre/exercícios básicos/ex1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
+        //Multiplique um númer por 2
 
         Scanner leitor = new Scanner(System.in);
 
