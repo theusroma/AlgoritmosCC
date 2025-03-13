4f5897beacdae6b219438225ee8cf7e0db7d282d
matheus o blasfemia <142417170+theusroma@users.noreply.github.com>
2025-03-13 13:00:43 -0300

    Create aluno.java

---
+++ 3 Semestre/objetos/matricula, nome, notas e media/aluno.java
@@ -0,0 +1,34 @@
+public class Aluno {
+
+    String nome;
+    float nota1, nota2;
+    int idade, matricula;
+
+    void verMedia(){
+        float media = (nota1 + nota2) / 2;
+        System.out.println("Média Final: " + media);
+    }
+
+    float media(){
+        return (nota1 + nota2) / 2;
+    }
+
+    void verMatricula(){
+        System.out.println("Matricula: " + matricula);
+    }
+
+    void verNome(){
+        System.out.println("Nome: " + nome);
+    }
+
+    void verNota1(){
+        System.out.println("Nota 1: " + nota1);
+    }
+
+    void verNota2(){
+        System.out.println("Nota 2: " + nota2);
+    }
+
+
+
+}
