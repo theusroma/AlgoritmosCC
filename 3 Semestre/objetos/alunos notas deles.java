e496b5245611f69003387070965a2fa964d94e14
matheus o blasfemia <142417170+theusroma@users.noreply.github.com>
2025-03-13 11:29:12 -0300

    Create alunos notas deles.java

---
+++ 3 Semestre/objetos/alunos notas deles.java
@@ -0,0 +1,69 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Aluno aluno1 = new Aluno();
+
+        int[] numeros = new int[2];
+        Aluno[] turma = new Aluno[5];
+
+        turma[0] = new Aluno();
+        turma[1] = new Aluno();
+
+        turma[0].nome = "Carlos";
+        turma[0].nota1 = 3.0f;
+        turma[0].nota2 = 1.0f;
+
+        turma[1].nome = "Maria";
+        turma[1].nota1 = 8.0f;
+        turma[1].nota2 = 1.3f;
+
+        for (int i=0; i<2; i++){
+            turma[i].verMedia();
+        }
+
+        turma[0].verMedia();
+
+        }
+    }
+
+
+
+
+//i
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Aluno aluno1 = new Aluno();
+
+        int[] numeros = new int[2];
+        Aluno[] turma = new Aluno[5];
+
+        turma[0] = new Aluno();
+        turma[1] = new Aluno();
+
+        turma[0].nome = "Carlos";
+        turma[0].nota1 = 3.0f;
+        turma[0].nota2 = 1.0f;
+
+        turma[1].nome = "Maria";
+        turma[1].nota1 = 8.0f;
+        turma[1].nota2 = 1.3f;
+
+        for (int i=0; i<2; i++){
+            turma[i].verMedia();
+        }
+
+        turma[0].verMedia();
+
+        }
+    }
+
+
+
+
+
+
+
