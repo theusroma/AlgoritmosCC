9971241a963993123ea17e4b2079e3fb96b25d9c
matheus o blasfemia <142417170+theusroma@users.noreply.github.com>
2025-03-14 11:16:18 -0300

    Update and rename 3 Semestre/objetos/outro só de exemplo/teste.java to 3 Semestre/objetos/carrro.java

---
+++ 3 Semestre/objetos/carrro.java
@@ -0,0 +1,14 @@
+public class Carro {
+    String modelo;
+    String marca;
+    int ano;
+
+    Carro(String modelo, int ano){
+//      modelo = "Branco";
+//      ano = 2020;
+        this.modelo = modelo;
+        this.ano = ano;
+        marca = "R";
+
+    }
+}
