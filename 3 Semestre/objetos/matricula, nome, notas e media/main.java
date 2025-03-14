d03fc7481c00469c25eb11ac62450699d8908a6b
matheus o blasfemia <142417170+theusroma@users.noreply.github.com>
2025-03-14 10:42:18 -0300

    Update main.java

--- 3 Semestre/objetos/matricula, nome, notas e media/main.java
+++ 3 Semestre/objetos/matricula, nome, notas e media/main.java
@@ -1,35 +1,58 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
 
-        Aluno[] turma = new Aluno[4];
+        Aluno[] turma = new Aluno[2];
+//
+//
+//        turma[0] = new Aluno();
+//        turma[1] = new Aluno();
+//        turma[2] = new Aluno();
+//        turma[3] = new Aluno();
 
-        turma[0] = new Aluno();
-        turma[1] = new Aluno();
-        turma[2] = new Aluno();
-        turma[3] = new Aluno();
+        for (int i=0; i < 2; i++){
+            turma[i] = new Aluno();
+            System.out.println("Digite a matrícula: ");
+            turma[i].matricula = input.nextInt();
+            input.nextLine();
 
-        turma[0].nome = "Carlos";
-        turma[0].matricula = 1111;
-        turma[0].nota1 = 3.0f;
-        turma[0].nota2 = 1.0f;
+            System.out.println("Digite o nome: ");
+            turma[i].nome = input.nextLine();
 
-        turma[1].nome = "Maria";
-        turma[1].matricula = 2222;
-        turma[1].nota1 = 8.0f;
-        turma[1].nota2 = 1.3f;
+            System.out.println("Digite a nota 1: ");
+            turma[i].nota1 = input.nextFloat();
+            input.nextLine();
 
-        turma[2].nome = "Letícia";
-        turma[2].matricula = 3333;
-        turma[2].nota1 = 7.0f;
-        turma[2].nota2 = 7.6f;
+            System.out.println("Digite a nota 2: ");
+            turma[i].nota2 = input.nextFloat();
+            input.nextLine();
+            System.out.println("");
+        }
 
-        turma[3].nome = "Matheus";
-        turma[3].matricula = 4444;
-        turma[3].nota1 = 4.5f;
-        turma[3].nota2 = 3.8f;
+//        turma[0].nome = "Carlos";
+//        turma[0].matricula = 1111;
+//        turma[0].nota1 = 3.0f;
+//        turma[0].nota2 = 1.0f;
+//
+//        turma[1].nome = "Maria";
+//        turma[1].matricula = 2222;
+//        turma[1].nota1 = 8.0f;
+//        turma[1].nota2 = 1.3f;
+//
+//        turma[2].nome = "Letícia";
+//        turma[2].matricula = 3333;
+//        turma[2].nota1 = 7.0f;
+//        turma[2].nota2 = 7.6f;
+//
+//        turma[3].nome = "Matheus";
+//        turma[3].matricula = 4444;
+//        turma[3].nota1 = 4.5f;
+//        turma[3].nota2 = 3.8f;
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 2; i++) {
             turma[i].verMatricula();
             turma[i].verNome();
             turma[i].verNota1();
@@ -48,6 +71,3 @@ public class Main {
 
     }
 }
-
-
-
