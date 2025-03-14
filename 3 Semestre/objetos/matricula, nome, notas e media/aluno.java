3e371df35b051db320fe8a854c9cdc78a1257a8b
matheus o blasfemia <142417170+theusroma@users.noreply.github.com>
2025-03-14 09:40:12 -0300

    Update aluno.java

--- 3 Semestre/objetos/matricula, nome, notas e media/aluno.java
+++ 3 Semestre/objetos/matricula, nome, notas e media/aluno.java
@@ -1,34 +1,71 @@
-public class Aluno {
+import java.util.Scanner;
 
-    String nome;
-    float nota1, nota2;
-    int idade, matricula;
+public class Main {
+    public static void main(String[] args) {
 
-    void verMedia(){
-        float media = (nota1 + nota2) / 2;
-        System.out.println("Média Final: " + media);
+        Scanner input = new Scanner(System.in);
+
+        Aluno[] turma = new Aluno[4];
+
+
+        turma[0] = new Aluno();
+        turma[1] = new Aluno();
+        turma[2] = new Aluno();
+        turma[3] = new Aluno();
+
+        for (int i=0; i < 4; i++){
+            System.out.println("Digite a matrícula: ");
+            turma[i].matricula = input.nextInt();
+            input.nextLine();
+
+            System.out.println("Digite o nome: ");
+            turma[i].nome = input.nextLine();
+
+            System.out.println("Digite a nota 1: ");
+            turma[i].nota1 = input.nextFloat();
+            input.nextLine();
+
+            System.out.println("Digite a nota 2: ");
+            turma[i].nota2 = input.nextFloat();
+            input.nextLine();
+        }
+
+//        turma[0].nome = "Carlos";
+//        turma[0].matricula = 1111;
+//        turma[0].nota1 = 3.0f;
+//        turma[0].nota2 = 1.0f;
+//
+//        turma[1].nome = "Maria";
+//        turma[1].matricula = 2222;
+//        turma[1].nota1 = 8.0f;
+//        turma[1].nota2 = 1.3f;
+//
+//        turma[2].nome = "Letícia";
+//        turma[2].matricula = 3333;
+//        turma[2].nota1 = 7.0f;
+//        turma[2].nota2 = 7.6f;
+//
+//        turma[3].nome = "Matheus";
+//        turma[3].matricula = 4444;
+//        turma[3].nota1 = 4.5f;
+//        turma[3].nota2 = 3.8f;
+
+        for (int i = 0; i < 4; i++) {
+            turma[i].verMatricula();
+            turma[i].verNome();
+            turma[i].verNota1();
+            turma[i].verNota2();
+            float media = turma[i].media();
+            if (media >= 6){
+                System.out.println("Aprovado (X)( )");
+            }
+            else{
+                System.out.println("Aprovado ( )(X)");
+            }
+            turma[i].verMedia();
+            System.out.println("");
+        }
+
+
     }
-
-    float media(){
-        return (nota1 + nota2) / 2;
-    }
-
-    void verMatricula(){
-        System.out.println("Matricula: " + matricula);
-    }
-
-    void verNome(){
-        System.out.println("Nome: " + nome);
-    }
-
-    void verNota1(){
-        System.out.println("Nota 1: " + nota1);
-    }
-
-    void verNota2(){
-        System.out.println("Nota 2: " + nota2);
-    }
-
-
-
 }
