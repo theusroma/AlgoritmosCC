bdb31d83d4cde6099a1482a13ed05f42addac958
matheus o blasfemia <142417170+theusroma@users.noreply.github.com>
2025-03-21 12:12:59 -0300

    Create main.java

---
+++ 3 Semestre/objetos/get & set/exemplo de get & set/main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Cofrinho meuCofre = new Cofrinho(); 
+
+        meuCofre.setMoedas(10); // Colocamos 10 moedas no cofre
+        System.out.println("Total de moedas no cofre: " + meuCofre.getMoedas()); // Pegamos o valor
+    }
+}
