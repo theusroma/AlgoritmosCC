a338f85a537974f519e18c13df13cc9daf92490d
Matheus Santos <142417170+theusroma@users.noreply.github.com>
2025-04-03 09:52:39 -0300

    Update main.java

--- 3 Semestre/objetos/Xadrez/main.java
+++ 3 Semestre/objetos/Xadrez/main.java
@@ -1,31 +1,56 @@
 public class Main {
+
     public static void main(String[] args) {
 
+
         Player p = new Player(2, 2);
+        Inimigo a = new Inimigo(5, 6);
+
+        Player[] personagens = new Player[2];
+        Inimigo[] inimigos = new Inimigo[2];
+
+        personagens[0] = new Player(1, 1);
+        personagens[1] = new Player(2, 2);
+
+
+        inimigos[0] = new Inimigo(3, 2);
+        inimigos[1] = new Inimigo(4 , 2);
+
+
 
         for (int linha = 0; linha < 5; linha++) {
-            for (int coluna = 0; coluna < 5; coluna++) {
-                System.out.print("[ . ]");
-            }
-            System.out.println();
-        }
 
-        System.out.println();
-        System.out.println();
-        System.out.println();
-
-        for (int linha = 0; linha < 5; linha++) {
             for (int coluna = 0; coluna < 5; coluna++) {
-                if (linha == p.x && coluna == p.y){
-                    System.out.print("[ X ]");
+
+                boolean tem = false;
+
+                for (int i = 0; i < personagens.length; i++) {
+
+
+                    if(personagens[i].posY == linha && personagens[i].posX == coluna){
+                        System.out.print("[ X ]");
+                        tem = true;
+                        break;
+                    }
+
+
+                    if (inimigos[i].posY == linha && inimigos[i].posX == coluna){
+                        System.out.print("[ A ]");
+                        tem = true;
+                        break;
+                    }
                 }
-                else{
+
+                if (!tem){
                     System.out.print("[ . ]");
                 }
 
+
             }
+
             System.out.println();
         }
 
     }
+
 }
