dafb6ffdd875ce0bee87a5fc3b619b83458e53af
Matheus Santos <142417170+theusroma@users.noreply.github.com>
2025-04-25 01:44:43 -0300

    Update Personagem.java

--- 3 Semestre/Herança/Jogo/Personagem.java
+++ 3 Semestre/Herança/Jogo/Personagem.java
@@ -1,24 +1,31 @@
 public class Personagem {
     String nome;
     float vida, dano;
-    Personagem(String nome, float vida, float dano){
+    Personagem(String nome, float vida){
         this.nome = nome;
         this.vida = vida;
-        this.dano = dano;
 
     }
     void mostrarPersonagem(){
         System.out.println("Personagem: "+this.nome);
         System.out.println("Vida: "+this.vida);
-        System.out.println("Dano: "+this.dano);
 
     }
 
-    void atacar(){
+    void atacar(Personagem alvo){
     }
 
-    void receberDano(){
-        this.vida = this.vida - this.dano;
+
+    void receberDano(float danoRecebido){
+        this.vida = this.vida - danoRecebido;
 
     }
+
+    void estaVivo(){
+        if (this.vida <= 0){
+            System.out.println("Está morto!");
+        }
+        else System.out.println("Está vivo!");
+    }
+
 }
